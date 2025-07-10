6319b0828b6bf4ecef7c50172c315c4893906240
jnchuhaiwebsite <18653115732@163.com>
2025-07-10 13:02:14 +0800

    Update UserMenu.vue

--- components/UserMenu.vue
+++ components/UserMenu.vue
@@ -9,8 +9,7 @@
         >
           <img
             :src="userDisplay?.imageUrl || '/default-avatar.png'"
-            :alt="userDisplay?.username ? `${userDisplay.username}'s Profile Picture - Hailuo02 Video` : 'Default User Avatar - Hailuo02 Video'"
-            loading="lazy"
+            :alt="userDisplay?.username ? `${userDisplay.username}'s Profile Picture - Hailuo02 Video` : 'Default User Avatar - Hailuo 02 Video'"
             class="w-10 h-10 rounded-full object-cover hover:opacity-80 transition-opacity"
           />
         </button>
@@ -111,7 +110,7 @@
                   d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
                 />
               </svg>
-              logout
+              Sign Out
             </button>
           </SignOutButton>
         </div>
